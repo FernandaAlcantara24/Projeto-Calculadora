e1d59510ecf0bf2c64694ea4a2c37809d04975a6
Fernanda Alcantara <fernanda.alcantara2407@gmail.com>
2022-01-14 19:52:35 -0300

    Calculadora

---
+++ ProjetoCalculadora/frmCalculadora.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace ProjetoCalculadora
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbHistorico = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnVirgula = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnBackspace = new System.Windows.Forms.Button();
+            this.btnAdicao = new System.Windows.Forms.Button();
+            this.btnSubtracao = new System.Windows.Forms.Button();
+            this.btnMultiplicacao = new System.Windows.Forms.Button();
+            this.btnDivisao = new System.Windows.Forms.Button();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.txbVisor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txbHistorico
+            // 
+            this.txbHistorico.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txbHistorico.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbHistorico.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txbHistorico.Location = new System.Drawing.Point(12, 26);
+            this.txbHistorico.Name = "txbHistorico";
+            this.txbHistorico.ReadOnly = true;
+            this.txbHistorico.Size = new System.Drawing.Size(406, 43);
+            this.txbHistorico.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.keycap_1_facebook;
+            this.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn1.Location = new System.Drawing.Point(12, 136);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(64, 63);
+            this.btn1.TabIndex = 2;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.keycap_2_facebook;
+            this.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn2.Location = new System.Drawing.Point(94, 136);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(64, 63);
+            this.btn2.TabIndex = 3;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.keycap_3_facebook;
+            this.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn3.Location = new System.Drawing.Point(175, 136);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(64, 63);
+            this.btn3.TabIndex = 4;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.keycap_4_facebook;
+            this.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn4.Location = new System.Drawing.Point(12, 222);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(64, 64);
+            this.btn4.TabIndex = 5;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.keycap_5_facebook;
+            this.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn5.Location = new System.Drawing.Point(94, 222);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(64, 64);
+            this.btn5.TabIndex = 6;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.keycap_6_facebook;
+            this.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn6.Location = new System.Drawing.Point(175, 222);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(64, 64);
+            this.btn6.TabIndex = 7;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.keycap_7_facebook;
+            this.btn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn7.Location = new System.Drawing.Point(12, 309);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(64, 64);
+            this.btn7.TabIndex = 8;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.keycap_8_facebook;
+            this.btn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn8.Location = new System.Drawing.Point(94, 309);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(64, 64);
+            this.btn8.TabIndex = 9;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.keycap_9_facebook;
+            this.btn9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn9.Location = new System.Drawing.Point(175, 309);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(64, 64);
+            this.btn9.TabIndex = 10;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btnVirgula
+            // 
+            this.btnVirgula.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.virgula;
+            this.btnVirgula.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnVirgula.Location = new System.Drawing.Point(175, 389);
+            this.btnVirgula.Name = "btnVirgula";
+            this.btnVirgula.Size = new System.Drawing.Size(64, 67);
+            this.btnVirgula.TabIndex = 11;
+            this.btnVirgula.UseVisualStyleBackColor = true;
+            this.btnVirgula.Click += new System.EventHandler(this.btnVirgula_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.C;
+            this.btnLimpar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLimpar.Location = new System.Drawing.Point(283, 136);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(64, 63);
+            this.btnLimpar.TabIndex = 12;
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnBackspace
+            // 
+            this.btnBackspace.BackgroundImage = global::ProjetoCalculadora.Properties.Resources._2b05;
+            this.btnBackspace.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBackspace.Location = new System.Drawing.Point(368, 136);
+            this.btnBackspace.Name = "btnBackspace";
+            this.btnBackspace.Size = new System.Drawing.Size(64, 63);
+            this.btnBackspace.TabIndex = 13;
+            this.btnBackspace.UseVisualStyleBackColor = true;
+            this.btnBackspace.Click += new System.EventHandler(this.btnBackspace_Click);
+            // 
+            // btnAdicao
+            // 
+            this.btnAdicao.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.Adicao;
+            this.btnAdicao.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAdicao.Location = new System.Drawing.Point(283, 222);
+            this.btnAdicao.Name = "btnAdicao";
+            this.btnAdicao.Size = new System.Drawing.Size(64, 64);
+            this.btnAdicao.TabIndex = 14;
+            this.btnAdicao.UseVisualStyleBackColor = true;
+            this.btnAdicao.Click += new System.EventHandler(this.btnAdicao_Click);
+            // 
+            // btnSubtracao
+            // 
+            this.btnSubtracao.BackgroundImage = global::ProjetoCalculadora.Properties.Resources._1;
+            this.btnSubtracao.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSubtracao.Location = new System.Drawing.Point(368, 222);
+            this.btnSubtracao.Name = "btnSubtracao";
+            this.btnSubtracao.Size = new System.Drawing.Size(64, 64);
+            this.btnSubtracao.TabIndex = 15;
+            this.btnSubtracao.UseVisualStyleBackColor = true;
+            this.btnSubtracao.Click += new System.EventHandler(this.btnSubtracao_Click);
+            // 
+            // btnMultiplicacao
+            // 
+            this.btnMultiplicacao.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.X;
+            this.btnMultiplicacao.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMultiplicacao.Location = new System.Drawing.Point(283, 309);
+            this.btnMultiplicacao.Name = "btnMultiplicacao";
+            this.btnMultiplicacao.Size = new System.Drawing.Size(64, 64);
+            this.btnMultiplicacao.TabIndex = 16;
+            this.btnMultiplicacao.UseVisualStyleBackColor = true;
+            this.btnMultiplicacao.Click += new System.EventHandler(this.btnMultiplicacao_Click);
+            // 
+            // btnDivisao
+            // 
+            this.btnDivisao.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.Divisao;
+            this.btnDivisao.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDivisao.Location = new System.Drawing.Point(368, 309);
+            this.btnDivisao.Name = "btnDivisao";
+            this.btnDivisao.Size = new System.Drawing.Size(64, 64);
+            this.btnDivisao.TabIndex = 17;
+            this.btnDivisao.UseVisualStyleBackColor = true;
+            this.btnDivisao.Click += new System.EventHandler(this.btnDivisao_Click);
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.igual2;
+            this.btnIgual.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnIgual.Location = new System.Drawing.Point(368, 395);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(64, 61);
+            this.btnIgual.TabIndex = 19;
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.BackgroundImage = global::ProjetoCalculadora.Properties.Resources.keycap_0_facebook;
+            this.btn0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn0.Location = new System.Drawing.Point(12, 389);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(146, 67);
+            this.btn0.TabIndex = 20;
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // txbVisor
+            // 
+            this.txbVisor.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txbVisor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbVisor.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txbVisor.Location = new System.Drawing.Point(12, 65);
+            this.txbVisor.Name = "txbVisor";
+            this.txbVisor.ReadOnly = true;
+            this.txbVisor.Size = new System.Drawing.Size(406, 57);
+            this.txbVisor.TabIndex = 21;
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProjetoCalculadora.Properties.Resources._04819cbc3da36e6542fcbd54a2723649__silver_background_texture_metal;
+            this.ClientSize = new System.Drawing.Size(444, 465);
+            this.Controls.Add(this.txbVisor);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.btnDivisao);
+            this.Controls.Add(this.btnMultiplicacao);
+            this.Controls.Add(this.btnSubtracao);
+            this.Controls.Add(this.btnAdicao);
+            this.Controls.Add(this.btnBackspace);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVirgula);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txbHistorico);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCalculadora";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbHistorico;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnVirgula;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnBackspace;
+        private System.Windows.Forms.Button btnAdicao;
+        private System.Windows.Forms.Button btnSubtracao;
+        private System.Windows.Forms.Button btnMultiplicacao;
+        private System.Windows.Forms.Button btnDivisao;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.TextBox txbVisor;
+    }
+}
+
---
+++ ProjetoCalculadora/frmCalculadora.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoCalculadora
+{
+    public partial class frmCalculadora : Form
+    {
+        string operacao = "";
+        double valorAnterior = 0;
+        bool primeiraOperacao = false;
+        double valorVisor = 0;
+        bool botaoIgual = false;
+
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender; // O objeto botão que foi clicado será carregado no botaoAcionado.
+            switch (botaoAcionado.Name)
+            {
+                case "btn1":                // Caso seja btn1
+                    txbVisor.Text += "1";  // A propriedade Text do visor receberá o número 1
+                    break;                // Parar a verificação.
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender;
+            switch (botaoAcionado.Name)
+            {
+                case "btn2":
+                    txbVisor.Text += "2";
+                    break;
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender;
+            switch (botaoAcionado.Name)
+            {
+                case "btn3":
+                    txbVisor.Text += "3";
+                    break;
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender;
+            switch (botaoAcionado.Name)
+            {
+                case "btn4":
+                    txbVisor.Text += "4";
+                    break;
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender;
+            switch (botaoAcionado.Name)
+            {
+                case "btn5":
+                    txbVisor.Text += "5";
+                    break;
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender;
+            switch (botaoAcionado.Name)
+            {
+                case "btn6":
+                    txbVisor.Text += "6";
+                    break;
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender;
+            switch (botaoAcionado.Name)
+            {
+                case "btn7":
+                    txbVisor.Text += "7";
+                    break;
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender;
+            switch (botaoAcionado.Name)
+            {
+                case "btn8":
+                    txbVisor.Text += "8";
+                    break;
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender;
+            switch (botaoAcionado.Name)
+            {
+                case "btn9":
+                    txbVisor.Text += "9";
+                    break;
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender;
+            switch (botaoAcionado.Name)
+            {
+                case "btn0":
+                    txbVisor.Text += "0";
+                    break;
+            }
+        }
+
+        private void btnVirgula_Click(object sender, EventArgs e)
+        {
+            Button botaoAcionado = (Button)sender;
+            switch (botaoAcionado.Name)
+            {
+                case "btnVirgula":
+                    txbVisor.Text += ",";
+                    break;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txbVisor.Clear();
+            txbHistorico.Clear();
+
+            valorAnterior = 0;
+            valorVisor = 0;
+
+            operacao = "";
+            primeiraOperacao = true;
+            botaoIgual = false;
+
+        }
+
+        private void btnBackspace_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text.Remove(txbVisor.Text.Length - 1);
+        }
+
+        private void btnAdicao_Click(object sender, EventArgs e)
+        {
+            if (primeiraOperacao)
+            {
+                valorAnterior = Convert.ToDouble(txbVisor.Text);
+
+                if (botaoIgual == false)
+                {
+                    txbHistorico.Text += txbVisor.Text;
+                }
+
+                txbVisor.Clear();
+                operacao = "+";
+                primeiraOperacao = false;
+            }
+            else
+            {
+                valorVisor = Convert.ToDouble(txbVisor.Text);
+                txbHistorico.Text += operacao + txbVisor.Text;
+                txbVisor.Text = Convert.ToString(valorAnterior + valorVisor);
+            }
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            valorVisor = Convert.ToDouble(txbVisor.Text);
+
+            txbHistorico.Text += operacao + txbVisor.Text;
+
+            txbVisor.Text = Convert.ToString(Calculo());
+
+            txbHistorico.Text += "=" + txbVisor.Text;
+
+            valorAnterior = Convert.ToDouble(txbVisor.Text);
+
+            botaoIgual = true;
+            primeiraOperacao = true;
+        }
+
+        public double Calculo()
+        {
+             switch(operacao)
+             {
+                case "+":
+                    valorAnterior = valorAnterior + valorVisor;
+                    break;
+
+                case "-":
+                    valorAnterior = valorAnterior - valorVisor;
+                    break;
+
+                case "*":
+                    valorAnterior = valorAnterior * valorVisor;
+                    break;
+
+                case "/":
+                    valorAnterior = valorAnterior / valorVisor;
+                    break;
+
+                default:
+                    break;
+            }
+            return valorAnterior;
+        }
+
+        private void btnSubtracao_Click(object sender, EventArgs e)
+        {
+            if (primeiraOperacao)
+            {
+                valorAnterior = Convert.ToDouble(txbVisor.Text);
+
+                if (botaoIgual == false)
+                {
+                    txbHistorico.Text += txbVisor.Text;
+                }
+
+                txbVisor.Clear();
+                operacao = "-";
+                primeiraOperacao = false;
+            }
+
+            else
+            {
+                valorVisor = Convert.ToDouble(txbVisor.Text);
+                txbHistorico.Text += operacao + txbVisor.Text;
+                txbVisor.Text = Convert.ToString(valorAnterior - valorVisor);
+            }
+        }
+
+        private void btnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            if (primeiraOperacao)
+            {
+                valorAnterior = Convert.ToDouble(txbVisor.Text);
+
+                if (botaoIgual == false)
+                {
+                    txbHistorico.Text += txbVisor.Text;
+                }
+
+                txbVisor.Clear();
+                operacao = "x";
+                primeiraOperacao = false;
+            }
+            else
+            {
+                valorVisor = Convert.ToDouble(txbVisor.Text);
+                txbHistorico.Text += operacao + txbVisor.Text;
+                txbVisor.Text = Convert.ToString(valorAnterior * valorVisor);
+            }
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            if (primeiraOperacao)
+            {
+                valorAnterior = Convert.ToDouble(txbVisor.Text);
+
+                if (botaoIgual == false)
+                {
+                    txbHistorico.Text += txbVisor.Text;
+                }
+
+                txbVisor.Clear();
+                operacao = "/";
+                primeiraOperacao = false;
+            }
+            else
+            {
+                valorVisor = Convert.ToDouble(txbVisor.Text);
+                txbHistorico.Text += operacao + txbVisor.Text;
+                txbVisor.Text = Convert.ToString(valorAnterior / valorVisor);
+            }
+        }
+    }
+}
